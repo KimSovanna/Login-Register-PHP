76f554b5eea65e35856a671b73a2499de71f5ab0
Kim Sovanna <KimSovanna@users.noreply.github.com>
2023-02-03 12:06:50 +0700

    Update config.php

--- config.php
+++ config.php
@@ -5,10 +5,17 @@ $user = "registeradmin";
 $pass = "password@123";
 $database = "register";
 
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn,NULL,NULL, "./DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, 'mydemoserver.mysql.database.azure.com', 'myadmin@mydemoserver', 'yourpassword', 'quickstartdb', 3306, NULL, MYSQLI_CLIENT_SSL);
+
 $conn = mysqli_connect($server, $user, $pass, $database);
 
-if (!$conn) {
-    die("<script>alert('Connection Failed.')</script>");
+if (mysqli_connect_errno()) {
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 ?>
